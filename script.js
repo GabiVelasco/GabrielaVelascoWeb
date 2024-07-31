160b180f9e17783de7ce9dcbe1cc6268cce75911
Gabriela Velasco <gabriela.luppe@gmail.com>
2024-07-31 13:11:18 +0200

    adjusting footer

--- script.js
+++ script.js
@@ -8,104 +8,59 @@ document.addEventListener('DOMContentLoaded', function() {
     const content = document.getElementById('content');
     const zitate = document.getElementById('zitate');
 
-    // Default content
-    const defaultContent = `
-        <h2>Willkommen</h2>
-        <p>Bitte wählen Sie einen Menüpunkt aus.</p>
-    `;
-
-    // About content
+    // Content Data
+    const defaultContent = '<h2>Willkommen!</h2><p>Ich bin Gabriela Velasco, Webentwicklerin.</p>';
     const aboutContent = `
-    <p style=padding-top:5px; padding-bottom: 5px;><br><br>
-      
-        - Angular<br>
-        - TypeScript<br>
-        - HTML<br>
-        - CSS<br>
-        - JavaScript<br>
-        - Firebase<br>
-        - Firestore<br>
-        - Python<br>
-        - React<br>
-        - PHP<br>
-       
-    </p>
-    `;
-
-    // Work content
+        <p style="padding-top:5px; padding-bottom: 5px;"><br><br>
+            - Angular<br>
+            - TypeScript<br>
+            - HTML<br>
+            - CSS<br>
+            - JavaScript<br>
+            - Firebase<br>
+            - Firestore<br>
+            - Python<br>
+            - React<br>
+            - PHP<br>
+        </p>`;
     const workContent = `
-        <h1>Die Integration von Tanz und Technologie</h1>
-        <p style="font-size:24px;">Mit einer einzigartigen Verbindung von Tanz und Technologie 
-            bringt Gabriela eine kreative Perspektive in die Welt der Informatik. 
-            <br><br>
-            Durch ihre Erfahrungen als Tänzerin und Choreografin integriert sie künstlerische Ausdrucksformen 
-            in ihre technologischen Projekte. Dieser kreative Ansatz ermöglicht es ihr, 
-            innovative Lösungen zu entwickeln und einzigartige Perspektiven in der sich ständig 
-            weiterentwickelnden Schnittstelle von Kunst und Technologie zu schaffen.
-        </p>
-    `;
-
-    // Kontakt content
+        <p><br>
+            - Erstellung von Webseiten und Webanwendungen.<br>
+            - Entwicklung und Umsetzung individueller Lösungen.<br>
+            - Leidenschaft für kreatives Design und sauberen Code.<br>
+            - Fokus auf Benutzerfreundlichkeit und Responsive Design.<br>
+            - Stetige Weiterbildung und Anwendung neuer Technologien.<br><br><br>
+        </p>`;
     const kontaktContent = `
-        <h2>Kontakt</h2>
-        <a href="mailto:gabriela.luppe@gmail.com"><p>Hier sind meine Kontaktinformationen...</p></a>
-        
-    `;
+        <p>Sie erreichen mich über die Social Media Links oder via 
+            <a href="mailto:gabriela.luppe@gmail.com">E-Mail.</a>
+        </p>`;
+    const logoContent = '<h2>Willkommen!</h2><p>Ich bin Gabriela Velasco, Webentwicklerin.</p>';
 
-    // Event listeners
-    aboutLink.addEventListener('click', function(event) {
-        event.preventDefault();
-        content.innerHTML = aboutContent;
-        zitate.style.display = 'none';
+    const aboutZitate = "Über mich.";
+    const workZitate = "Meine Arbeit.";
+    const kontaktZitate = "Kontakt.";
+
+    // Event Listeners
+    aboutLink.addEventListener('click', function() {
+        updateContent(aboutContent, aboutZitate);
     });
 
-    workLink.addEventListener('click', function(event) {
-        event.preventDefault();
-        content.innerHTML = workContent;
-        zitate.style.display = 'none';
+    workLink.addEventListener('click', function() {
+        updateContent(workContent, workZitate);
     });
 
-    kontaktLink.addEventListener('click', function(event) {
-        event.preventDefault();
-        content.innerHTML = kontaktContent;
-        zitate.style.display = 'none';
+    kontaktLink.addEventListener('click', function() {
+        updateContent(kontaktContent, kontaktZitate);
     });
 
-    logoLink.addEventListener('click', function(event) {
-        event.preventDefault();
-        content.innerHTML = defaultContent;
-        zitate.style.display = 'block';
+    logoLink.addEventListener('click', function() {
+        updateContent(logoContent, defaultContent);
     });
-});
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Function to update zitate content
-    function updateZitate(content) {
-        document.getElementById('zitate').innerHTML = content;
+    // Update Function
+    function updateContent(newContent, newZitate) {
+        content.innerHTML = newContent;
+        zitate.innerHTML = newZitate;
     }
-
-    // Event listeners for navigation links
-    document.getElementById('logo-link').addEventListener('click', function(event) {
-        event.preventDefault();
-        updateZitate('<br><br>Als Webentwicklerin <br>tanze ich mit dem Code, <br>um die digitale Welt zu verschönern.');
-    });
-
-    document.getElementById('work-link').addEventListener('click', function(event) {
-        event.preventDefault();
-        updateZitate('<br><br>Mission - Die Integration von Tanz und Technologie');
-    });
-
-    document.getElementById('about-link').addEventListener('click', function(event) {
-        event.preventDefault();
-        updateZitate('<br><br>Mehr über mich');
-    });
-
-    document.getElementById('kontakt-link').addEventListener('click', function(event) {
-        event.preventDefault();
-        updateZitate('<br><br>Kontakt');
-    });
-
-    // Set default content for initial page load
-    updateZitate('<br><br>Als Webentwicklerin <br>tanze ich mit dem Code, <br>um die digitale Welt zu verschönern.');
 });
-
